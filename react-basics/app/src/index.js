0e9541e3d20bf1b796501ba7336eb32bde07a22a
shun198 <shunhiroseluvmri@gmail.com>
2023-06-03 07:57:23 +0900

    👍 update files

--- react-basics/app/src/index.js
+++ react-basics/app/src/index.js
@@ -1,21 +1,9 @@
-//package.jsonからパッケージをimport
-// コンポーネントという
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
 // public/index.htmlのdivタグ内のrootを参照
-const root = ReactDOM.createRoot(document.getElementById('root'));
 // src/App.jsからApp()メソッドを参照
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
