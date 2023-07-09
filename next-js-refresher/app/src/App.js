9ba173859a465d02b074847f27712748e831aeb2
shun198 <shunhiroseluvmri@gmail.com>
2023-07-09 17:10:27 +0900

    👍 add components

--- next-js-refresher/app/src/App.js
+++ next-js-refresher/app/src/App.js
@@ -1,5 +1,14 @@
+import Todo from './components/Todo';
+
 function App() {
-  return <div>Hello!</div>;
+  return (
+    <div>
+      <h1>Todo Lists</h1>
+      <Todo text="Learn React" />
+      <Todo text="Master React" />
+      <Todo text="Explore Full Course React" />
+    </div>
+  );
 }
 
 export default App;
