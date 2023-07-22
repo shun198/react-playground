a4af69cb2a6fd98af7811742b004792f8dda8955
shun198 <shunhiroseluvmri@gmail.com>
2023-07-22 14:54:04 +0900

    👍 add card

--- next-js-refresher/app/src/components/meetups/NewMeetupForm.js
+++ next-js-refresher/app/src/components/meetups/NewMeetupForm.js
@@ -1,26 +1,57 @@
+import { useRef } from 'react';
 import Card from '../ui/Card';
 import classes from './NewMeetupForm.module.css';
 
-function NewMeetupForm() {
+function NewMeetupForm(props) {
+  const titleInputRef = useRef();
+  const imageInputRef = useRef();
+  const addressInputRef = useRef();
+  const descriptionInputRef = useRef();
+
+  function submitHandler(event) {
+    // event.preventDefaultメソッドは、submitイベントの発生元であるフォームが持つデフォルトの動作をキャンセルするメソッド
+    // フォームが持つデフォルトの動作とは、フォームの内容を指定したURLへ送信するという動作
+    // https://developer.mozilla.org/ja/docs/Web/API/Event/preventDefault
+    event.preventDefault();
+    const enterdTitle = titleInputRef.current.value;
+    const enterdImage = imageInputRef.current.value;
+    const enterdAddress = addressInputRef.current.value;
+    const enterdDescription = descriptionInputRef.current.value;
+
+    const meetupData = {
+      title: enterdTitle,
+      image: enterdImage,
+      address: enterdAddress,
+      description: enterdDescription,
+    };
+
+    props.onAddMeetup(meetupData);
+  }
+
   return (
     <Card>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor="title">Meetup Title</label>
-          <input type="text" required id="title" />
+          <input type="text" required id="title" ref={titleInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor="image">Meetup Image</label>
-          <input type="url" required id="image" />
+          <input type="url" required id="image" ref={imageInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor="address">Meetup Address</label>
-          <input type="text" required id="address" />
+          <input type="text" required id="address" ref={addressInputRef} />
         </div>
       </form>
       <div className={classes.control}>
         <label htmlFor="description">Meetup Description</label>
-        <textarea id="description" required rows="5"></textarea>
+        <textarea
+          id="description"
+          required
+          rows="5"
+          ref={descriptionInputRef}
+        ></textarea>
       </div>
       <div className={classes.actions}>
         <button>Add Meetup</button>
--- next-js-refresher/app/src/pages/NewMeetup.js
+++ next-js-refresher/app/src/pages/NewMeetup.js
@@ -1,11 +1,25 @@
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
+  function addMeetupHandler(meetupData) {
+    // firebase内にmeetupsテーブルを作成
+    // firebaseAPIを使用する際は最後に.jsonを追加する必要あり
+    fetch(
+      'https://next-js-refresher-default-rtdb.firebaseio.com/meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+  }
   return (
-    <div>
+    <section>
       <h1> Add New Meetup Page</h1>
-      <NewMeetupForm />
-    </div>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
   );
 }
 
