a929ac83b0519d3de1a3e4548187d0820dcaae63
shun198 <shunhiroseluvmri@gmail.com>
2023-07-22 10:56:57 +0900

    👍 add NewMeetupForm

--- next-js-refresher/app/src/App.js
+++ next-js-refresher/app/src/App.js
@@ -1,13 +1,29 @@
-import Todo from './components/Todo';
+// import Todo from './components/Todo';
+import { Route, Switch } from 'react-router-dom';
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavoritesPage from './pages/Favorites';
+import Layout from './components/layout/Layout';
 
 function App() {
   return (
-    <div>
-      <h1>Todo Lists</h1>
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
+      {/* <h1>Todo Lists</h1>
       <Todo text="Learn React" />
       <Todo text="Master React" />
-      <Todo text="Explore Full Course React" />
-    </div>
+      <Todo text="Explore Full Course React" /> */}
+    </Layout>
   );
 }
 
---
+++ next-js-refresher/app/src/pages/NewMeetup.js
@@ -0,0 +1,12 @@
+import NewMeetupForm from '../components/meetups/NewMeetupform';
+
+function NewMeetupPage() {
+  return (
+    <div>
+      <h1> Add New Meetup Page</h1>
+      <NewMeetupForm />
+    </div>
+  );
+}
+
+export default NewMeetupPage;
