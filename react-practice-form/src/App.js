df5ec22700dc32baa94b8f729e6080e8331e355c
shun198 <shunhiroseluvmri@gmail.com>
2023-09-02 21:54:31 +0900

    👍 add form

--- react-practice-form/src/App.js
+++ react-practice-form/src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
-function App() {
+function App(props) {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uEmployeeNumber, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        {
+          employeenumber: uEmployeeNumber,
+          age: uAge,
+          id: Math.random().toString(),
+        },
+      ];
+    });
+  };
+
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
--- react-practice-form/src/components/Users/AddUser.js
+++ react-practice-form/src/components/Users/AddUser.js
@@ -1,23 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '../UI/Card';
+import Button from '../UI/Button';
 import classes from './AddUser.module.css';
+import ErrorModal from '../UI/ErrorModal';
 
 const AddUser = (props) => {
+  const [enteredEmployeeNumber, setenteredEmployeeNumber] = useState('');
+  const [enteredAge, setEnteredAge] = useState('');
+  const [error, setError] = useState();
+
   const addUserHandler = (event) => {
     event.preventDefault();
+    console.log(enteredEmployeeNumber, enteredAge);
+    if (enteredEmployeeNumber.length !== 8) {
+      setError({
+        title: '社員番号が不正です',
+        message: '社員番号は8桁にしてください',
+      });
+      return;
+    }
+    props.onAddUser(enteredEmployeeNumber, enteredAge);
+    setenteredEmployeeNumber('');
+    setEnteredAge('');
+  };
+
+  const employeeNumberChangeHandler = (event) => {
+    setenteredEmployeeNumber(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+
+  const errorHandler = () => {
+    setError(null);
   };
 
   return (
-    <Card clasName={classes.input}>
-      <form onSubmit={addUserHandler}>
-        {/* JSXではforの代わりにhtmlForを使う */}
-        <label htmlFor="username">Username</label>
-        <input id="username" placeholder="username" type="text"></input>
-        <label htmlFor="username">Age(Years)</label>
-        <input id="age" placeholder="number" type="number"></input>
-        <button type="submit">Add User</button>
-      </form>
-    </Card>
+    <div>
+      {/* errorがobjectなら{}内をレンダリングする */}
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
+      <Card className={classes.input}>
+        {/* submitボタンを押すとaddUserHandlerが起動する */}
+        <form onSubmit={addUserHandler}>
+          {/* JSXではforの代わりにhtmlForを使う */}
+          <label htmlFor="employeeNumber" className="text-red-500">
+            社員番号
+          </label>
+          <input
+            id="employeeNumber"
+            placeholder="社員番号"
+            type="text"
+            value={enteredEmployeeNumber}
+            onChange={employeeNumberChangeHandler}
+          ></input>
+          <label htmlFor="age">Age(Years)</label>
+          <input
+            id="age"
+            placeholder="number"
+            type="number"
+            value={enteredAge}
+            onChange={ageChangeHandler}
+          ></input>
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </div>
   );
 };
 
