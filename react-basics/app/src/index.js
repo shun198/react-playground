2861ae9da121b732f1f8af2d1fc447a27eedec08
shun198 <shunhiroseluvmri@gmail.com>
2024-08-25 13:16:43 +0900

    👍 delete files

--- react-basics/app/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-import './index.css';
-import App from './App';
-// public/index.htmlのdivタグ内のrootを参照
-// src/App.jsからApp()メソッドを参照
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
